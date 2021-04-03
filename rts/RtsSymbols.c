61b57cfcfbff9f5590e3da3dfeedbfd32ca7164e
Sven Tennie <sven.tennie@gmail.com>
2021-04-03 19:35:34 +0200

    Use Info Table Provenances to decode cloned stack (#18163)
    Emit an Info Table Provenance Entry (IPE) for every stack represeted info table
    if -finfo-table-map is turned on.
    
    To decode a cloned stack, lookupIPE() is used. It provides a mapping between
    info tables and their source location.
    
    Please see these notes for details:
    
    - [Stacktraces from Info Table Provenance Entries (IPE based stack unwinding)]
    - [Mapping Info Tables to Source Positions]
    
    Metric Increase:
        T12545

--- rts/RtsSymbols.c
+++ rts/RtsSymbols.c
@@ -1016,6 +1016,7 @@
       SymI_HasProto(registerInfoProvList)                               \
       SymI_HasProto(lookupIPE)                                          \
       SymI_HasProto(sendCloneStackMessage)                              \
+      SymI_HasProto(decodeClonedStack)                                  \
       RTS_USER_SIGNALS_SYMBOLS                                          \
       RTS_INTCHAR_SYMBOLS
 
